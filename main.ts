2d1b954fef82abe8f175b2c8befcb26da6630250
SJJeon04 <64895747+SJJeon04@users.noreply.github.com>
2021-06-21 11:49:37 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+let hangman = ""
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+let random = randint(0, 4)
+if (random == 0) {
+    hangman = "cledor"
+} else if (random == 1) {
+    hangman = "basket"
+} else if (random == 2) {
+    hangman = "eagle"
+} else if (random == 3) {
+    hangman = "water"
+} else if (random == 4) {
+    hangman = "radio"
+}
 basic.forever(function () {
-	
+    while (true) {
+    	
+    }
 })
