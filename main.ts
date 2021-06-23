2d31beae9d268285ad2a86f6c0cad79a1226d5ca
SJJeon04 <64895747+SJJeon04@users.noreply.github.com>
2021-06-23 10:00:45 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,4 +1,73 @@
+input.onButtonPressed(Button.A, function () {
+    alphabet += 1
+    basic.showString("" + (문자열_리스트[alphabet]))
+})
+input.onButtonPressed(Button.AB, function () {
+    list_count = 1
+    count += -1
+    if (hangman.includes(문자열_리스트[alphabet]) == true) {
+        문자열_리스트2[hangman.indexOf(문자열_리스트[alphabet])] = 문자열_리스트[alphabet]
+        for (let index = 0; index < 4; index++) {
+            basic.showString("" + (문자열_리스트2[list_count]))
+            list_count += 1
+        }
+    } else {
+        if (count == 5) {
+            basic.showLeds(`
+                . . . . .
+                # # # # #
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+        } else if (count == 4) {
+            basic.showLeds(`
+                . . . . .
+                . . # # #
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+        } else if (count == 3) {
+            basic.showLeds(`
+                . . . . .
+                . . # . .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+        } else if (count == 2) {
+            basic.showLeds(`
+                . . . . .
+                . . # . .
+                . . # . .
+                . . . # .
+                . . . . #
+                `)
+        } else if (count == 1) {
+            basic.showLeds(`
+                . . . . .
+                . . # . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        } else {
+            basic.showString("Game Over")
+            game.gameOver()
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    alphabet += -1
+    basic.showString("" + (문자열_리스트[alphabet]))
+})
+let list_count = 0
 let hangman = ""
+let 문자열_리스트2: string[] = []
+let 문자열_리스트: string[] = []
+let count = 0
+let alphabet = 0
 basic.showLeds(`
     . . # . .
     # # # # #
@@ -6,11 +75,15 @@ basic.showLeds(`
     . # . # .
     # . . . #
     `)
+alphabet = 0
+count = 6
+문자열_리스트 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+문자열_리스트2 = ["_", "_", "_", "_", "_"]
 let random = randint(0, 4)
 if (random == 0) {
-    hangman = "cledor"
+    hangman = "alien"
 } else if (random == 1) {
-    hangman = "basket"
+    hangman = "black"
 } else if (random == 2) {
     hangman = "eagle"
 } else if (random == 3) {
@@ -18,8 +91,5 @@ if (random == 0) {
 } else if (random == 4) {
     hangman = "radio"
 }
-basic.forever(function () {
-    while (true) {
-    	
-    }
-})
+basic.pause(500)
+basic.clearScreen()
